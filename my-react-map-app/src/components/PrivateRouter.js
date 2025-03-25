4fac72ad4b0c23cc3d67d54d4c00e1eea6d1fa4b
vijayalakshmiTanniru <vijayalakshmitanniru98@gmail.com>
2025-03-25 07:27:02 +0530

    Update PrivateRouter.js

--- my-react-map-app/src/components/PrivateRouter.js
+++ my-react-map-app/src/components/PrivateRouter.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
 
+function PrivateRoute({ children }) {
+  const isAuthenticated = localStorage.getItem('token'); // Or your authentication check
+  const location = useLocation();
+
+  return isAuthenticated ? (
+    children
+  ) : (
+    <Navigate to="/login" state={{ from: location }} replace />
+  );
+}
+
+export default PrivateRoute;
