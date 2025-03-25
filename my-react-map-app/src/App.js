28f68ee3c67f907f3af1b5eb20e9d40a1e5764bf
vijayalakshmiTanniru <vijayalakshmitanniru98@gmail.com>
2025-03-25 07:28:01 +0530

    Update App.js

--- my-react-map-app/src/App.js
+++ my-react-map-app/src/App.js
@@ -1,1 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import MapView from './components/MapView';
+import PrivateRoute from './components/PrivateRoute';
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<PrivateRoute> <Dashboard /> </PrivateRoute>} />
+        <Route path="/dashboard" element={<PrivateRoute> <Dashboard /> </PrivateRoute>} />
+        <Route path="/map/:cardId" element={<PrivateRoute> <MapView /> </PrivateRoute>} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
